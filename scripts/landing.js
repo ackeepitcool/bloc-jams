f8cac0969f79bd16b37f4285e37052e1e924efdd
Aaron Chen <agchen2@gmail.com>
2017-09-11 14:43:19 -0400

    Checkpoint 28 JQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(point) {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+  var revealPoint = function() {
+       // #7
+       $(this).css({
+           opacity: 1,
+           transform: 'scaleX(1) translateY(0)'
+       });
+     };
+     $.each($('.point'), revealPoint);
 };
 
 var animatePoints = function(points) {
     forEach(points, revealPoint);
 };
 
-window.onload = function() {
-  if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+ $(window).load(function() {
+   if ($(window).height() > 950) {
+         animatePoints();
+   var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event) {
-    if (pointsArray[0].getBoundingClientRect().top <= 500) {
-          animatePoints(pointsArray);
+
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
       }
    });
-}
+});
